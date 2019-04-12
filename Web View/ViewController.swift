19c54ec68264fd544ea8525717faab6d41ac324e
Andi Aljabar <andi.aljabar30@gmail.com>
2019-04-12 09:25:04 +0800

    Initial Commit

---
+++ Web View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Web View
+//
+//  Created by Andi Aljabar Mudding on 12/04/19.
+//  Copyright © 2019 Andi Aljabar Mudding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Web ViewTests/Web_ViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  Web_ViewTests.swift
+//  Web ViewTests
+//
+//  Created by Andi Aljabar Mudding on 12/04/19.
+//  Copyright © 2019 Andi Aljabar Mudding. All rights reserved.
+//
+
+import XCTest
+@testable import Web_View
+
+class Web_ViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
